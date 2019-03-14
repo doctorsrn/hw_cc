5098c2beb6a9a6e03737832ed6d34e6d10b76e7a
Benzzzxxx <37198424+Benzzzxxx@users.noreply.github.com>
2019-03-14 22:07:48 +0800

    Add files via upload

--- SDK_python/CodeCraft-2019/src/callmovezp.py
+++ SDK_python/CodeCraft-2019/src/callmovezp.py
@@ -21,6 +21,7 @@ def main():
     readdata(carfile,carmap)
     readdata(roadfile,roadmap)
     readdata(crossfile,crossmap)
+    crossidtransfer(crossmap,crossidmap)
     car_size=len(carmap)
     cross_size=len(crossmap)
     road_size=len(roadmap)
@@ -29,7 +30,7 @@ def main():
     createnvir(cross_size)
 
     #求调度时间
-    value=CalScheduleTime(answerfile,crossmap,roadmap,carmap,cross_size,road_size,car_size,roadmat)   #参数：answer.txt路径、路口字典、道路字典、车辆字典、路口数目、道路数目、车辆数目、路网
+    value=CalScheduleTime(answerfile,crossmap,crossidmap,roadmap,carmap,cross_size,road_size,car_size,roadmat)   #参数：answer.txt路径、路口字典、道路字典、车辆字典、路口数目、道路数目、车辆数目、路网
     print(value)
     
 if __name__=="__main__":
